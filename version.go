2f4900758a2b7478803b2323e45ecb91705c4765
Michael D Henderson <mdhender@users.noreply.github.com>
2026-09-22 20:34:23 -0600

    Add version management with semver package

---
+++ version.go
@@ -0,0 +1,19 @@
+package es
+
+import "github.com/maloquacious/semver"
+
+var (
+	version = semver.Version{
+		Major:      0,
+		Minor:      1,
+		Patch:      0,
+		PreRelease: "alpha",
+
+		// Automatically populate build metadata with commit info
+		Build: semver.Commit(), // Uses Git commit hash from build info
+	}
+)
+
+func Version() semver.Version {
+	return version
+}
